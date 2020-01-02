44b6aa633b5d0b4d003808133664550e5e65b4a6
Parker Amundsen <parkera13@gmail.com>
2020-01-01 16:18:54 -0800

    Initial Commit

---
+++ GraphLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphLib
+//
+//  Created by parker amundsen on 1/1/20.
+//  Copyright © 2020 Parker Buhler Amundsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
